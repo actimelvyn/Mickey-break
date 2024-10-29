dbe44d0595f5d94a07488845893ed63669beb8f8
Ranu <jk.verbiese@gmail.com>
2024-10-29 16:01:52 -0400

    PickUP Modify

--- Assets/Scripts/Script_PickUp_Light.cs
+++ Assets/Scripts/Script_PickUp_Light.cs
@@ -6,24 +6,35 @@ public class Script_PickUp_Light : MonoBehaviour
 {
 
     public GameObject LightOnPlayer;
+    public GameObject PickUpText;
 
     // Start is called before the first frame update
     void Start()
     {
         LightOnPlayer.SetActive(false);
+        PickUpText.SetActive(false);
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            PickUpText.SetActive(true);
+
             if (Input.GetKeyUp(KeyCode.E)) 
             {
                 this.gameObject.SetActive(false);
 
                 LightOnPlayer.SetActive(true);
+
+                PickUpText.SetActive(false);
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PickUpText.SetActive(false);
     }
 
 }
