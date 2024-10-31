4b413abae2674c2120a5f51704875db849d82e9c
Ranu <jk.verbiese@gmail.com>
2024-10-31 12:52:39 -0400

    Script Grab_Hold_Drop

---
+++ Assets/Scripts/Script_ObjectGrabbable.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_ObjectGrabbable : MonoBehaviour
+{
+
+    private Rigidbody objectRigidbody;
+    private Transform objectGrabPointTansform;
+
+    private void Awake()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Grab(Transform objectGrabPointTansform)
+    {
+        this.objectGrabPointTansform = objectGrabPointTansform;
+        objectRigidbody.useGravity = false;
+    }
+
+    public void Drop()
+    {
+        this.objectGrabPointTansform = null;
+        objectRigidbody.useGravity = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (objectGrabPointTansform != null)
+        {
+            float lerpSpeed = 10f;
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTansform.position, Time.deltaTime * lerpSpeed);
+            objectRigidbody.MovePosition(newPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Script_Player_PickUpNDrop.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player_PickUpNDrop : MonoBehaviour
+{
+    [SerializeField] private Transform PlayerCameraTransform;
+
+    [SerializeField] private Transform objectGrabPointTansform;
+
+    [SerializeField] private LayerMask PickUpLayerMask;
+
+    private ObjectGrabbable objectGrabbable;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (objectGrabbable == null)
+            {
+
+                float pickUpDistance = 2f;
+
+                if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, PickUpLayerMask)) ;
+                {
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTansform);
+                    }
+                }
+            }
+            else
+            {
+                objectGrabbable.Drop();
+                objectGrabbable = null;
+            }
+        }
+    }
+}
