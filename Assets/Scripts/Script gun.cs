4effac3232584afa92a25476af58db14b1173bd2
Zarden <105550691+Zardren@users.noreply.github.com>
2024-11-07 20:06:39 -0500

    gun indentation

--- Assets/Scripts/Script gun.cs
+++ Assets/Scripts/Script gun.cs
@@ -2,36 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scriptgun : MonoBehaviour
-{
-    public float damage = 10f;
-    public float range = 100f;
+//public class scriptgun : monobehaviour
+//{
+//    public float damage = 10f;
+//    public float range = 100f;
 
-    public Camera FPScam;
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            Shoot();
-        }
-    }
+//    public camera fpscam;
+//    update is called once per frame
+//    void update()
+//    {
+//        if (input.getmousebuttondown(1))
+//        {
+//            shoot();
+//        }
+//    }
 
-    void Shoot()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(FPScam.transform.position, FPScam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
+//    void shoot()
+//    {
+//        raycasthit hit;
+//        if (physics.raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+//        {
+//            debug.log(hit.transform.name);
 
-           Target target = hit.transform.GetComponent<Target>();
+//            target target = hit.transform.getcomponent<target>();
 
-            if (target != null)
-            {
-                target.TakeDamage(damage);
+//            if (target != null)
+//            {
+//                target.takedamage(damage);
 
-            }
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
