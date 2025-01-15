5c73e00c84a67489335aab5be532a56ed4c9b7d6
actimelvyn <165931115+actimelvyn@users.noreply.github.com>
2025-01-14 20:41:14 -0500

    Merge branch 'main' of https://github.com/actimelvyn/Mickey-break

--- Assets/Scipt_final_door.cs
+++ Assets/Scipt_final_door.cs
@@ -1,29 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
-public class Scipt_final_door : MonoBehaviour
+public class Script_Final_Door : MonoBehaviour
 {
-    private int interactable = 1;
-    
-    private void OnTriggerEnter(Collider other)
+    public Script_PickUP_Key keyPickup; // Reference to the key pickup script
+    private Animator animator;          // Reference to the door animator
+
+    private void Start()
     {
-        if (other.CompareTag("Player") && interactable == 2)
+        // Get the Animator component attached to the door
+        animator = GetComponent<Animator>();
+
+        // Check if the key pickup script is assigned
+        if (keyPickup == null)
         {
-            GetComponent<BoxCollider>().isTrigger = true ;
-            Destroy(gameObject);
+            Debug.LogError("Script_PickUP_Key not assigned! Please assign it in the Inspector.");
+        }
+
+        // Check if the Animator component is attached
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on the door!");
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the player entered the trigger and canOpenDoor is true
+        if (other.CompareTag("Player") && keyPickup != null && keyPickup.canOpenDoor)
+        {
+            Debug.Log("Player triggered the door. Opening door...");
+
+            // Trigger the door opening animation
+            animator.SetTrigger("open");
+        }
+        else if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player triggered the door, but canOpenDoor is false.");
+        }
     }
 }
